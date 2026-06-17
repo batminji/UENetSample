9f0e3e21152c916c02e7e3d11b5c3e78a4a2da53
BATMINJI <minjiconan@naver.com>
2026-06-17 11:26:44 +0900

    위젯 프로퍼티 및 함수 바인딩

--- Source/UENetSample/Lobby/LobbyWidgetBase.cpp
+++ Source/UENetSample/Lobby/LobbyWidgetBase.cpp
@@ -2,4 +2,52 @@
 
 
 #include "LobbyWidgetBase.h"
+#include "Components/Button.h"
+#include "Components/EditableTextBox.h"
+#include "Components/TextBlock.h"
+#include "Components/ScrollBox.h"
 
+void ULobbyWidgetBase::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+	
+	if (StartButton)
+	{
+		StartButton->OnClicked.AddDynamic(this, &ULobbyWidgetBase::OnStartButtonClicked);
+	}
+	if (SendButton)
+	{
+		SendButton->OnClicked.AddDynamic(this, &ULobbyWidgetBase::OnSendButtonClicked);
+	}
+	if (InputText)
+	{
+		InputText->OnTextCommitted.AddDynamic(this, &ULobbyWidgetBase::OnInputTextCommitted);
+		InputText->OnTextChanged.AddDynamic(this, &ULobbyWidgetBase::OnInputTextChanged);
+	}
+
+}
+
+void ULobbyWidgetBase::OnStartButtonClicked()
+{
+}
+
+void ULobbyWidgetBase::OnSendButtonClicked()
+{
+	
+}
+
+void ULobbyWidgetBase::OnInputTextCommitted(const FText& Text, ETextCommit::Type CommitMethod)
+{
+}
+
+void ULobbyWidgetBase::OnInputTextChanged(const FText& Text)
+{
+}
+
+void ULobbyWidgetBase::UpdateLeftTime(const int32 InLeftTime)
+{
+}
+
+void ULobbyWidgetBase::UpdateConnectionCount(const int32 InConnectionCount)
+{
+}
--- Source/UENetSample/Lobby/LobbyWidgetBase.h
+++ Source/UENetSample/Lobby/LobbyWidgetBase.h
@@ -6,12 +6,50 @@
 #include "Blueprint/UserWidget.h"
 #include "LobbyWidgetBase.generated.h"
 
-/**
- * 
- */
+class UButton;
+class UEditableTextBox;
+class UTextBlock;
+class UScrollBox;
+
 UCLASS()
 class UENETSAMPLE_API ULobbyWidgetBase : public UUserWidget
 {
 	GENERATED_BODY()
+
+public:
+	virtual void NativeOnInitialized() override;
+
+	UFUNCTION()
+	void OnStartButtonClicked();
+	UFUNCTION()
+	void OnSendButtonClicked();
+	UFUNCTION()
+	void OnInputTextCommitted(const FText& Text, ETextCommit::Type CommitMethod);
+	UFUNCTION()
+	void OnInputTextChanged(const FText& Text);
+
+	UFUNCTION(BlueprintCallable)
+	void UpdateLeftTime(const int32 InLeftTime);
+
+	UFUNCTION(BlueprintCallable)
+	void UpdateConnectionCount(const int32 InConnectionCount);
 	
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (BindWidget))
+	TObjectPtr<UButton> StartButton;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (BindWidget))
+	TObjectPtr<UButton> SendButton;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (BindWidget))
+	TObjectPtr<UEditableTextBox> InputText;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (BindWidget))
+	TObjectPtr<UTextBlock> ConnectionCount;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (BindWidget))
+	TObjectPtr<UTextBlock> LeftTime;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (BindWidget))
+	TObjectPtr<UScrollBox> ChatBox;
 };
